dbbe34832b58ef2021d90de7af54b95318126feb
avinashtuscano <avinashtuscano@gmail.com>
2014-08-04 22:54:45 +0100

    signup page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,14 +8,15 @@ describe "User pages" do
     let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
 
-    it { should have_content(user.name) }
-    it { should have_title(user.name) }
+    it { has_content?(user.name) }
+    it { has_title?(user.name) }
   end
 
-  describe "signup page" do
+describe "signup page" do
     before { visit signup_path }
 
-    it { should have_content('Sign up') }
-    it { should have_title(full_title('Sign up')) }
+    it { has_content?('Sign up') }
+    it { has_title?(full_title('Sign up')) }
   end
+  
 end
